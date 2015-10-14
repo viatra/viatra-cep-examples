49cccce850d698b2cc5478f6ca69ac4887598a0f
Istvan David <goerenh@gmail.com>
2015-10-14 15:24:08 +0200

    Renamed the adapter, added comments

--- emf.notification/org.eclipse.viatra.cep.notification.test/src/org/eclipse/viatra/cep/notification/test/cases/BaseTests.java
+++ emf.notification/org.eclipse.viatra.cep.notification.test/src/org/eclipse/viatra/cep/notification/test/cases/BaseTests.java
@@ -3,7 +3,7 @@ package org.eclipse.viatra.cep.notification.test.cases;
 import org.apache.log4j.Level;
 import org.eclipse.viatra.cep.core.api.engine.CEPEngine;
 import org.eclipse.viatra.cep.core.metamodels.automaton.EventContext;
-import org.eclipse.viatra.cep.emf.notification.integration.NotificationAdapter;
+import org.eclipse.viatra.cep.emf.notification.integration.EcoreNotificationAdapter;
 import org.eclipse.viatra.cep.notification.test.CepFactory;
 import org.eclipse.viatra.cep.notification.test.model.carmodel.Car;
 import org.eclipse.viatra.cep.notification.test.model.carmodel.CarmodelFactory;
@@ -13,14 +13,14 @@ import org.junit.Test;
 
 public class BaseTests {
 	private Car car;
-	private NotificationAdapter notificationAdapter;
+	private EcoreNotificationAdapter notificationAdapter;
 	private CEPEngine cepEngine;
 
 	@Before
 	public void setUp() throws Exception {
 		car = CarmodelFactory.eINSTANCE.createCar();
 		cepEngine = CEPEngine.newEngine().eventContext(EventContext.CHRONICLE).rules(CepFactory.getInstance().allRules()).prepare();
-		notificationAdapter = new NotificationAdapter(car, cepEngine);
+		notificationAdapter = new EcoreNotificationAdapter(car, cepEngine);
 		notificationAdapter.setDebugLevel(Level.DEBUG);
 	}
 
