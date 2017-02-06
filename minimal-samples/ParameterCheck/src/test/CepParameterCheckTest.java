7266919026aa8f269c2b148c738ad12f9ee42d4f
bergmann <bergmann@mit.bme.hu>
2017-02-06 15:10:27 +0100

    Extended example to illustrate problem with incomplete binding

--- minimal-samples/ParameterCheck/src/test/CepParameterCheckTest.java
+++ minimal-samples/ParameterCheck/src/test/CepParameterCheckTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import vepl.CepFactory;
 import vepl.events.FIRST_Event;
 import vepl.events.SECOND_Event;
+import vepl.events.THIRD_Event;
 
 public class CepParameterCheckTest {
 	
@@ -26,12 +27,20 @@ public class CepParameterCheckTest {
 	public void test() {
 		FIRST_Event first = CepFactory.getInstance().createFIRST_Event();
 		SECOND_Event second = CepFactory.getInstance().createSECOND_Event();
+		THIRD_Event third_A = CepFactory.getInstance().createTHIRD_Event();
+		THIRD_Event third_B = CepFactory.getInstance().createTHIRD_Event();
 		
 		first.setRight(true);
 		second.setRight(true);
+		third_A.setFoo(100);
+		third_A.setRight(true);
+		third_B.setFoo(200);
+		third_B.setRight(false);
 		
 		firstEventStream.push(first);
 		firstEventStream.push(second);
+		firstEventStream.push(third_B);
+		firstEventStream.push(third_A);
 	}
 	
 }
